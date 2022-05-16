ae3ff6825500d3219fecf5cc2d2b483b8d0ed4ec
Majed Althonayan <97466204+MajedAlthonayan@users.noreply.github.com>
2022-05-16 21:39:53 +0100

    playing around with dynamic values

--- react-blog/src/App.js
+++ react-blog/src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 
 function App() {
+
+  const title = 'Welcome to the new blog!'
+  const likes = 50;
   return (
     <div className="App">
       <div className="content">
-        <h1>App Content</h1>
+        <h1>{title}</h1>
+        <p>Likes: {likes}</p>
+        <p>dislikes: {5}</p>
       </div>
     </div>
   );
