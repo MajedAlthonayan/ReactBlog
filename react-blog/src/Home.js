69d53d0dcc82746a9707431ecb7a23c238916ce6
Majed Althonayan <97466204+MajedAlthonayan@users.noreply.github.com>
2022-05-18 18:28:24 +0100

    moved the logic of printing the blog names into a new component to allow me to filter which blogs to display

--- react-blog/src/Home.js
+++ react-blog/src/Home.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import BlogList from './BlogList';
 
 const Home = () => {
 
@@ -17,12 +18,9 @@ const Home = () => {
 
     return (
         <div className="home">
-            {blogs.map((blog) => (
-                <div className="blog-preview" key={blog.id}>
-                    <h2>{blog.title}</h2>
-                    <p>Written by {blog.author}</p>
-                </div>
-            ))}
+        <BlogList blogs={blogs} title="All Teams:" />
+        <br/>
+        <BlogList blogs={blogs.filter((blog) => blog.id === 1 || blog.id === 2 || blog.id === 3)} title="Top Teams:" />
         </div>
 
     );
