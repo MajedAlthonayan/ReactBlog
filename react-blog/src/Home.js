124a59e66c2ce478d2ab7eec0bd8b28510e9e3df
Majed Althonayan <97466204+MajedAlthonayan@users.noreply.github.com>
2022-05-18 18:11:03 +0100

    Adding the blog previews to the home page

--- react-blog/src/Home.js
+++ react-blog/src/Home.js
@@ -1,20 +1,28 @@
 import { useState } from 'react';
 
 const Home = () => {
-   
-    const [name, setName] = useState('mario') ;
 
+    const [blogs, setBlogs] = useState([
+        { title: 'Ferrari', body: 'lorem ipsum', author: 'Majed', id: 1 },
+        { title: 'Mercedes', body: 'lorem ipsum', author: 'Majed', id: 2 },
+        { title: 'Red Bull', body: 'lorem ipsum', author: 'Majed', id: 3 },
+        { title: 'Mclaren', body: 'lorem ipsum', author: 'Majed', id: 4 },
+        { title: 'Alpine', body: 'lorem ipsum', author: 'Majed', id: 5 },
+        { title: 'Aston Martin', body: 'lorem ipsum', author: 'Majed', id: 6 },
+        { title: 'Alpha Tauri', body: 'lorem ipsum', author: 'Majed', id: 7 },
+        { title: 'Alfa Romeo', body: 'lorem ipsum', author: 'Majed', id: 8 },
+        { title: 'Williams', body: 'lorem ipsum', author: 'Majed', id: 9 },
+        { title: 'Haas', body: 'lorem ipsum', author: 'Majed', id: 10 },
+    ]);
 
-    const handleClick = () => {
-       setName('luigi');
-    }
     return (
         <div className="home">
-            <h2>Homepage</h2>
-            <p>{name}</p>
-
-            <button onClick={handleClick}>Click me</button>
-
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                </div>
+            ))}
         </div>
 
     );
