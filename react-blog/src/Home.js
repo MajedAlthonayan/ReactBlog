be856897ab483bc02e97ca783ba5aa89534115a0
Majed Althonayan <97466204+MajedAlthonayan@users.noreply.github.com>
2022-05-20 19:38:36 +0100

    added error handlng

--- react-blog/src/Home.js
+++ react-blog/src/Home.js
@@ -6,22 +6,32 @@ const Home = () => {
     const [blogs, setBlogs] = useState(null);
 
     const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
 
 
     useEffect(() => {
         setTimeout(() => {
             fetch('http://localhost:8000/blogs')
-                .then(res => {
-                    return res.json();
-                })
-                .then(data => {
-                    setBlogs(data);
-                    setIsPending(false);
-                })
+                    .then(res => {
+                        if(!res.ok){
+                            throw Error("Could not Fetch Data!");
+                        }
+                        return res.json();
+                    })
+                    .then(data => {
+                        setBlogs(data);
+                        setIsPending(false);
+                        setError(null);
+                    })
+                    .catch(err => {
+                        setIsPending(false);
+                        setError(err.message);
+                    })
         }, 1000);
     }, []);
     return (
         <div className="home">
+            {error && <div>{ error }</div>}
             {isPending && <div>Loading...</div>}
             {/* Doesn't bother if first is false, therefore null is not passed to bloglist */}
             {blogs && <BlogList blogs={blogs} title="All Teams:" />}
