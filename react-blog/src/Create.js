36e04a13fa90a57877dd0f4390ee9630991a2ba6
Majed Althonayan <97466204+MajedAlthonayan@users.noreply.github.com>
2022-05-24 18:18:06 +0100

    added a loading button as well as a POST request which actually adds new blogs

--- react-blog/src/Create.js
+++ react-blog/src/Create.js
@@ -4,41 +4,52 @@ const Create = () => {
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
     const [author, setAuthor] = useState('Majed');
+    const [isPending, setIsPending] = useState(false);
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        const blog = {title, body, author};
-        
+        const blog = { title, body, author };
+
+        setIsPending(true);
+
+        fetch('http://localhost:8000/blogs', {
+            method: 'POST',
+            headers: { "content-Type": "application/json" },
+            body: JSON.stringify(blog)
+        }).then(() => {
+            setIsPending(false);
+        })
     }
 
     return (
         <div className="create">
-        <h2>Add a new Blog</h2>
-        <form onSubmit={handleSubmit}>
-            <label>Blog Title</label>
-            <input 
-                type="text" 
-                required
-                value={title}
-                onChange ={(e) => setTitle(e.target.value)}
-            />
-            <label>Blog Body</label>
-            <textarea
-                required
-                value={body}
-                onChange ={(e) => setBody(e.target.value)}
-            ></textarea>
-            <label>Blog Author</label>
-            <select
-                 value={author}
-                 onChange ={(e) => setAuthor(e.target.value)}
-            >
-                <option value="Majed">Majed</option>
-            </select>
-            <button>Add Blog</button>
-        </form>
+            <h2>Add a new Blog</h2>
+            <form onSubmit={handleSubmit}>
+                <label>Blog Title</label>
+                <input
+                    type="text"
+                    required
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+                <label>Blog Body</label>
+                <textarea
+                    required
+                    value={body}
+                    onChange={(e) => setBody(e.target.value)}
+                ></textarea>
+                <label>Blog Author</label>
+                <select
+                    value={author}
+                    onChange={(e) => setAuthor(e.target.value)}
+                >
+                    <option value="Majed">Majed</option>
+                </select>
+                {!isPending && <button>Add Blog</button>}
+                {isPending && <button disabled>Adding Blog...</button>}
+            </form>
         </div>
-      );
+    );
 }
- 
+
 export default Create;
