96ef9dfed5af67786634dce9be77c2c4a68e6817
Majed Althonayan <97466204+MajedAlthonayan@users.noreply.github.com>
2022-05-24 18:58:52 +0100

    added some css so that each team is represented with its own colour

--- react-blog/src/BlogList.js
+++ react-blog/src/BlogList.js
@@ -8,7 +8,7 @@ const BlogList = ({ blogs, title, handleDelete }) => {
             {blogs.map((blog) => (
                 <div className="blog-preview" key={blog.id}>
                     <Link to={`/blogs/${blog.id}`}>
-                        <h2>{blog.title}</h2>
+                        <h2 className={"id" + blog.id}>{blog.title}</h2>
                         <p>Written by {blog.author}</p>
                     </Link>
                 </div>
